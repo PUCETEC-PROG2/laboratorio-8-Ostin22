3dcafbf3c6128fe5955b55a053776974848ca289
Ostin22 <rrcarrion@puce.edu.ec>
2024-07-27 09:19:04 -0500

    codigo pokemon

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon_id"),
     path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
-    path('login/', views.CustomLoginView.as_view(), name='login')
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path("edit_pokemon/<int:id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("delete_pokemon/<int:id>", views.delete_pokemon, name="delete_pokemon")
 ]
 
 #Con .as_view convertimos una clase en una vista, recuerda que anteriormente usabamos funciones
--- pokedex/views.py
+++ pokedex/views.py
@@ -3,9 +3,10 @@ from django.template import loader
 
 from .models import Pokemon
 from pokedex.forms import PokemonForm
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
+
 
 
 
@@ -38,9 +39,31 @@ def add_pokemon(request):
     else:
         form = PokemonForm()
         
-    return render(request, 'add_pokemon.html', {'form':form})
+    return render(request, 'pokemon_form.html', {'form':form})
 
 #Crearemos la vista del login, heredando el login de nuestro administrador.
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
+   
+#Funcion para editar el pokemon 
+@login_required
+def edit_pokemon(request, id):
+    pokemon = get_object_or_404(Pokemon, pk = id)
+    if request.method == 'POST':
+        form = PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = PokemonForm(instance=pokemon)
+        
+    return render(request, 'pokemon_form.html', {'form':form})
+
+
+@login_required
+
+def delete_pokemon(request, id):
+    pokemon = get_object_or_404(Pokemon, pk = id)
+    pokemon.delete()
+    return redirect("pokedex:index")
